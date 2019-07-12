88188c54c4360a20339e2a3dd68509d84d61d6e9
bayuuarf <37804147+bayuuarf@users.noreply.github.com>
2019-07-12 13:59:20 +0700

    Add files via upload

---
+++ Receiver/Receiver.ino
@@ -0,0 +1,29 @@
+#define laserreceiver 4
+#define buzzer 3
+void setup() {
+pinMode(laserreceiver,INPUT);
+pinMode(buzzer,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  
+if(digitalRead(laserreceiver)==HIGH){
+  
+Serial.println("Ada Kapal yang Mendekat");
+
+tone(buzzer, 400); //gelombang suara sebesar 400 Hz
+
+delay(1000); //Suara tersebut selama 1 detik  
+
+noTone(buzzer); //Suara akan berhenti
+
+delay(1000); //Berhenti selama 1 detik
+
+  }
+  else{
+    
+Serial.println("Tidak ada Kapal yang Mendekat");
+  
+    }
+}
---
+++ Transmitter/Transmitter.ino
@@ -0,0 +1,9 @@
+#define lasertransmitter 4
+void setup() {
+pinMode(lasertransmitter,OUTPUT);
+Serial.begin(9600);
+digitalWrite(lasertransmitter,HIGH);
+}
+
+void loop() {
+}
